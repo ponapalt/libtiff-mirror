1f79cbc484ff68908fb59da8671e2509b9c56643
Bob Friesenhahn <graphicsmagick.project@gmail.com>
2026-09-16 22:02:13 +0000

    Update autogen.sh to optionally obtain config.guess/config.sub from adjacent gnulib

--- autogen.sh
+++ autogen.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+#
+# Libtiff Autotools bootstrap script.
+#
+# See the `HOWTO-RELEASE` file for the details of how this script may
+# be used as part of libtiff maintenance, and/or read the comments
+# below.
+#
 
 retval=0
 
@@ -6,18 +13,23 @@ set -x
 
 autoreconf --install --force || retval=$?
 
-# Get latest config.guess and config.sub from upstream master since
-# these are often out of date.  This requires network connectivity and
-# sometimes the site is down, a failure here does not result in
-# failure of the whole script.
-for file in config.guess config.sub
-do
-    echo "$0: getting $file..."
-    wget --timeout=5 -O config/$file.tmp \
-      "https://git.savannah.gnu.org/cgit/config.git/plain/${file}" \
-      && mv -f config/$file.tmp config/$file \
-      && chmod a+x config/$file
-    rm -f config/$file.tmp
-done
+# The latest 'gitlog-to-changelog', 'config.guess', and 'config.sub'
+# files are in the git repository at
+# 'https://git.savannah.gnu.org/git/gnulib.git' and may be found in
+# its 'build-aux' subdirectory.
+#
+# Check-out gnulib adjacent to this source tree so this script can
+# update the build-aux files.
+#   e.g. git clone https://git.savannah.gnu.org/git/gnulib.git
+# Then make sure that the gnulib clone is updated prior to running this script.
+
+build_aux=../gnulib/build-aux
+if [ -d ${build_aux} ] ; then
+    printf "%s\n" "Updating build-aux files..."
+    for f in config.guess config.sub
+    do
+        cp -p ${build_aux}/${f} config/
+    done
+fi
 
 exit $retval
